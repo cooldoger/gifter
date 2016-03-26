171c2df2bb83c06a1d0d2d3e5397cf8362be1a10
zjay <zjay@a9.com>
2016-03-26 16:58:06 -0700

    Fix unittest build

--- tst/com/cooldoger/gifter/GifterTest.java
+++ tst/com/cooldoger/gifter/GifterTest.java
@@ -1,11 +1,16 @@
 package com.cooldoger.gifter;
 
+import java.nio.file.Path;
+import java.nio.file.FileSystems;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GifterTest {
     @Test
-     public void testCalculateAppriasal() {
-         assertEquals(500, 600);
-     }
+    public void testLoadDataSuccess() throws Exception {
+        Gifter app = new Gifter();
+        Path inputPath = FileSystems.getDefault().getPath("tst/res", "example.txt");
+        int num = app.loadData(inputPath);
+        assertEquals(num, 3);
+    }
 }
