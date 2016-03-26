2bb15623d30accd83d71992282ac9b12a0b36395
zjay <zjay@a9.com>
2016-03-26 16:17:44 -0700

    Init unittest

---
+++ tst/com/cooldoger/gifter/GifterTest.java
@@ -0,0 +1,11 @@
+package com.cooldoger.gifter;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GifterTest {
+    @Test
+     public void testCalculateAppriasal() {
+         assertEquals(500, 600);
+     }
+}
